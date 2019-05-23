481240419f68bf8059816e29f7d3ad73e5774cc4
raymondmar61 <raym61@hotmail.com>
2019-05-22 20:17:35 -0700

    upload

--- chapter03describingdatawithstatistics.py
+++ chapter03describingdatawithstatistics.py
@@ -136,9 +136,123 @@ x1 = donations
 y1 = [calculate_mean(donations) for n in range(0,len(donations))]
 x2 = donations2
 y2 = [calculate_mean(donations2) for n in range(0,len(donations2))]
-plot(x1)
-plot(y1)
-plot(x2)
-plot(y2)
-legend(["Donations 1","Mean 1","Donations 2","Mean 2"]) #legend() function shows legend
-#show()
+# plot(x1)
+# plot(y1)
+# plot(x2)
+# plot(y2)
+# legend(["Donations 1","Mean 1","Donations 2","Mean 2"]) #legend() function shows legend
+# show()
+
+#Pearson correlation coefficient or correlation coefficient.  It measures the strength of the linear relationship and not a nonlinear relationship.  The coefficient is between -1 and 1 inclusive.  A 0 indicates there's no linear correlation between the two quantities; however, there could be a nonlinear relationship.  A coefficient of 1 indicates a strong positive linear correlation.  A coefficient of -1 indicates a strong negative correlation.
+#Correlation doesn't imply causation.
+ziplist1 = [1, 2, 3]
+ziplist2 = [4, 5, 6]
+for x, y in zip(ziplist1, ziplist2):
+	print(x, y)
+'''
+1 4
+2 5
+3 6
+'''
+def correlationcoefficient(x, y):
+	n = len(x)
+	#find the sum of the products
+	products = []
+	for x1, y1 in zip(x, y):
+		products.append(x1*y1)
+	sumproducts = sum(products)
+	sumx = sum(x)
+	sumy = sum(y)
+	squaredsumx = sumx**2
+	squaredsumy = sumy**2
+
+	xsquare = []
+	for eachx in x:
+		xsquare.append(eachx**2)
+	#find the sum
+	xsquaresum = sum(xsquare)
+	ysquare = []
+	for eachy in y:
+		ysquare.append(eachy**2)
+	#find the sum
+	ysquaresum = sum(ysquare)
+
+	#calculate correlation
+	numerator = (n*sumproducts)-(sumx*sumy)
+	denominatorterm1 = (n*xsquaresum) - squaredsumx
+	denominatorterm2 = (n*ysquaresum) - squaredsumy
+	denominator = (denominatorterm1*denominatorterm2)**0.5
+	correlation = numerator/denominator
+	return correlation
+highschoolgrades1 = [90, 92, 95, 96, 87, 87, 90, 95, 98, 96]
+collegeadmissiontestscores1 = [85, 87, 86, 97, 96, 88, 89, 98, 98, 87]
+print(correlationcoefficient(highschoolgrades1,collegeadmissiontestscores1)) #print 0.3183785775683751.  Some correlation, not a strong one.  Its scatter plot shows data points not a straight line.
+highschoolmathgrades = [83, 85, 84, 96, 94, 86, 87, 97, 97, 85]
+collegeadmissiontestscores1 = [85, 87, 86, 97, 96, 88, 89, 98, 98, 87]
+print(correlationcoefficient(highschoolmathgrades,collegeadmissiontestscores1)) #print 0.9989633063220916.  Strong correlation.  Its scatter plot shows data points almost a straight line.
+
+#Scatter Plot
+import matplotlib.pyplot as plt
+scatterx = [1, 2, 3, 4]
+scattery = [2, 4, 6, 8]
+print(correlationcoefficient(scatterx,scattery)) #print 1.0
+plt.scatter(scatterx, scattery)
+plt.show()
+
+#Crash course Python code read data from a text file or import text file.
+with open("mydata.txt") as f:
+	for eachf in f:
+		print(eachf, end="") #print the numbers in mydata.txt in its own line
+print("\n")
+with open("mydata.txt") as f:	
+	print("second with open int(eachf) instead of eachf, end=""")
+	for eachf in f:
+		print(int(eachf)) #print the numbers in mydata.txt in its own line
+def readfile(filename):
+	numberslist = []
+	with open(filename) as f:
+		for eachf in f:
+			numberslist.append(int(eachf))
+	return numberslist
+#print(readfile("mydata.txt")) #print ['100\n', '60\n', '70\n', '900\n', '100\n', '200\n', '500\n', '500\n', '503\n', '600\n', '1000\n', '1200'] when numberslist.append(eachf)
+print(readfile("mydata.txt")) #print [100, 60, 70, 900, 100, 200, 500, 500, 503, 600, 1000, 1200] when numberslist.append(int(eachf))
+averagedata = readfile("mydata.txt")
+print(calculatemean(averagedata)) #print 411.75.  calculatemean function above at line 11
+
+#Crash course Python code read csv file
+import csv
+with open("numbers.csv") as f:
+	reader = csv.reader(f)
+	print(reader) #print <_csv.reader object at 0x7fec02007d68>
+	next(reader)
+	print(next(reader)) #print ['10', '100']
+	next(reader)
+	print(next(reader)) #print ['22', '484']
+with open("numbers.csv") as f:
+	reader = csv.reader(f)
+	next(reader)
+	print(reader) #print <_csv.reader object at 0x7fec02007d68>
+	print(next(reader)) #print ['10', '100']
+	next(reader)
+	print(reader) #print <_csv.reader object at 0x7fec02007d68>
+	print(next(reader)) #print ['22', '484']
+with open("numbers.csv") as f:
+	reader = csv.reader(f)
+	next(reader)
+	for eachreader in reader:
+		print(eachreader[0], eachreader[1]) #print 10 100\n 9 81\n 22 484
+def readcsv(filename):
+	xvalues = []
+	yvalues = []
+	with open(filename) as f:
+		reader = csv.reader(f)
+		next(reader)
+		for eachreader in reader:
+			xvalues.append(int(eachreader[0]))
+			yvalues.append(int(eachreader[1]))
+		return xvalues, yvalues
+x, y = readcsv("numbers.csv")
+plt.scatter(x, y)
+plt.xlabel("plt.xlabel Number")
+plt.ylabel("plt.ylabel Square")
+plt.show()
