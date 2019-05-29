d18dce5bdb7e8931b32ac39e687d8a2b33bdcef2
raymondmar61 <raym61@hotmail.com>
2019-05-28 19:20:34 -0700

    upload

--- chapter05playingwithsetsandprobability.py
+++ chapter05playingwithsetsandprobability.py
@@ -108,4 +108,142 @@ for eachtenthsetpowerthree in tenthsetpowerthree:
 (3, 3, 2)
 (3, 3, 3)
 '''
-#RM:  start page 131 Probability.
+print("\n")
+
+#Uniform probability all outcomes equally likely
+#Probability terms.  An experiment is the test.  An experiment is called a trial.  A sample space is all possible outcomes denoted S.  An event is a set of outcomes denoted E.  RM:  An event is the number of experiments based on the sample size.
+def uniformdistributionprobability(event, samplespace):
+	return len(event)/len(samplespace)
+#probability roll a 3 on a six-sided die
+samplespace = [1, 2, 3, 4, 5, 6]
+event = [3]
+print(uniformdistributionprobability(event, samplespace)) #print 0.16666666666666666
+#probability roll a prime number or an odd number
+samplespace = [1, 2, 3, 4, 5, 6]
+primenumber = {2, 3, 5}
+oddnumber = {1, 3, 5}
+event = primenumber.union(oddnumber)
+print(event) #print {1, 2, 3, 5}
+print(uniformdistributionprobability(event, samplespace)) #print 0.6666666666666666
+#probability roll a prime number and an odd number
+samplespace = [1, 2, 3, 4, 5, 6]
+primenumber = FiniteSet(2, 3, 5)
+oddnumber = [1, 3, 5]
+oddnumber = FiniteSet(*oddnumber)
+event = primenumber.intersect(oddnumber)
+print(event) #print {3, 5}
+print(uniformdistributionprobability(event, samplespace)) #print 0.3333333333333333
+#random numbers
+from random import randint
+print(randint(1,6)) #prints random number between 1 and 6 inclusive
+def rolladicetototalscore(totalscore):
+	diceroll = 0
+	rollnumber = 1
+	while diceroll <= totalscore:
+		diceroll = diceroll + randint(1,6)
+		if diceroll >= totalscore:
+			return rollnumber, diceroll
+		else:
+			rollnumber += 1
+totalscore = 20
+print("Score of {} reached in {} rolls.".format(rolladicetototalscore(totalscore)[1],rolladicetototalscore(totalscore)[0])) #print Score of 20 reached in 6 rolls.  RM:  number of rolls is random diceroll = diceroll + randint(1,6)
+def targetscoreprobability(sidedie, numberofrolls, targetscore):
+	diesides = [n for n in range(1,sideddie+1)]
+	diesides = FiniteSet(*diesides)
+	#print("Dice sides",diesides)
+	samplesize = diesides**numberofrolls
+	#print("Sample Size",samplesize)
+	successfulrolls = []  #RM:  I could use a counter for successfulrolls.  I choose to count the targetscores in a list to comprehend.
+	for eachsamplesize in samplesize:	
+		#print(eachsamplesize, sum(eachsamplesize))
+		if sum(eachsamplesize) == targetscore:
+			#print(eachsamplesize, sum(eachsamplesize))
+			successfulrolls.append(eachsamplesize)
+	successfuloutcomescount = len(successfulrolls)
+	samplesizecount = len(samplesize)
+	probability = successfuloutcomescount/samplesizecount
+	return probability
+sideddie = 6
+numberofrolls = 5
+targetscore = 25
+print("Probability of exact target score {} rolling a {}-sided die {} times is {}.".format(targetscore, sideddie, numberofrolls, targetscoreprobability(sideddie,numberofrolls,targetscore)))  #print Probability of exact target score 25 rolling a 6-sided die 5 times is 0.016203703703703703.
+sideddie = 20
+numberofrolls = 4
+targetscore = 80
+print("Probability of exact target score {} rolling a {}-sided die {} times is {}.".format(targetscore, sideddie, numberofrolls, targetscoreprobability(sideddie,numberofrolls,targetscore)))  #print Probability of exact target score 80 rolling a 20-sided die 4 times is 6.25e-06.
+
+#Nonuniform probability outcomes nonequally likely.  Draw a probability number line representing the possible outcomes.
+from random import random
+print(random())  #prints random floatingpoint number between 0 and 1 exclusive
+#probability a coin is heads 2/3 and tails 1/3
+def flipcoin():
+	#2/3 heads, 1/3 tails
+	coinflip = random()
+	if coinflip < 2/3:
+		return "Heads"
+	else:
+		return "Tails"
+for n in range(0,19):
+	print(flipcoin())
+'''
+Heads
+Heads
+Heads
+Heads
+Heads
+Tails
+Heads
+Heads
+Heads
+Heads
+Heads
+Heads
+Tails
+Heads
+Heads
+Heads
+Tails
+Heads
+Heads
+'''
+# dollarbills = [5, 10, 20, 50]
+# probabilities = [1/6, 1/6, 1/3, 1/3]
+# dollarbillsprobability = []
+# initializeprobability = 0
+# for p in probabilities:
+# 	initializeprobability+=p
+# dollarbillsprobability.append(initializeprobability)
+# print(dollarbillsprobability)
+
+def fictionalfreeatm():
+	dollarbills = [5, 10, 20, 50]
+	probabilities = [1/6, 1/6, 1/3, 1/3]
+	dollarbillsprobability = []
+	initializeprobability = 0
+	for p in probabilities:
+		initializeprobability+=p
+		dollarbillsprobability.append(initializeprobability)
+	#print(dollarbillsprobability)
+	probability = random()
+	for index, eachdollarbillsprobability in enumerate(dollarbillsprobability):
+		#print(index, eachdollarbillsprobability)
+		if probability < eachdollarbillsprobability:
+			return probability, index, dollarbills[index]
+atmvisits = 20
+counter = 1
+amountwithdrawn = 0
+while counter <= atmvisits:
+	withdrawal = fictionalfreeatm()
+	print(withdrawal)
+	amountwithdrawn+=withdrawal[2]
+	counter+=1
+print(amountwithdrawn)
+'''
+(0.10568177477900953, 0, 5)
+(0.6719005340142075, 3, 50)
+(0.24341150124107103, 1, 10)
+(0.4094235719966657, 2, 20)
+(0.6714461798290673, 3, 50)
+print(amountwithdrawn)-->135
+'''
+
